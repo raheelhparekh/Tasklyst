c4986740a719eed6343a481b4f56d994a9d8fa1d
raheelhparekh <rhparekh2003@gmail.com>
2025-05-14 21:05:22 +0530

    feat: add email functionality and file upload middleware
    - Added mailgen and nodemailer for email generation and sending.
    - Implemented multer middleware for handling file uploads.

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,7 +1,16 @@
 const asyncHandler = (requestHandler) => {
+  // return function() line 13 down there
   return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next).catch((err) => next(err)));
+    Promise.resolve(requestHandler(req, res, next)
+    .catch((err) => next(err)));
   };
 };
 
 export { asyncHandler };
+
+// the returned function is written down which is the same done as above
+
+// function test(req,res,next){
+//   Promise.resolve(()=>{})
+//   .catch()
+// }
---
+++ src/utils/mail.js
@@ -0,0 +1,81 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendMail = async (options) => {
+  var mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      // Appears in header & footer of e-mails
+      name: "Task Manager",
+    },
+  });
+
+  // Generate an HTML email with the provided contents
+  var emailHtml = mailGenerator.generate(options.mailGenContent);
+
+  // Generate the plaintext version of the e-mail (for clients that do not support HTML)
+  var emailText = mailGenerator.generatePlaintext(options.mailGenContent);
+
+  // Create a transporter for SMTP
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP.USERNAME,
+    port: process.env.MAILTRAP.PORT,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASSWORD,
+    },
+  });
+
+  const mail = {
+    from: 'anyemail@email.com', // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: emailText, // plain text body
+    html: emailHtml,
+  };
+
+  try {
+    await transporter.sendMail(mail)
+  } catch (error) {
+    console.log("error sending emails",error)
+    
+  }
+};
+
+const emailVerificationMailGenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro:
+        "Welcome to Task Manager! We're very excited to have you on board.",
+      action: {
+        instructions: "To get started with our app, please click here:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Confirm your account",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+const forgotPasswordMailGenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Change Your Password ?",
+      action: {
+        instructions: "To change your password, please click here:",
+        button: {
+          color: "#22BC66", // Optional action button color
+          text: "Reset Password",
+          link: passwordResetUrl,
+        },
+      },
+    },
+  };
+};
