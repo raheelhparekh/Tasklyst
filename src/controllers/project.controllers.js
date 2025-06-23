802c731970a91a90bb7c2b09004cc14950689b35
raheelhparekh <rhparekh2003@gmail.com>
2025-06-23 23:56:57 +0530

    feat: project controller logic added : get, create, update, delete

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,14 +1,120 @@
+import { Project } from "../models/project.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-errors.js";
+import { ApiResponse } from "../utils/api-response.js";
 
-const getProject = asyncHandler(async (req, res) => {});
+const getAllProjectsOfUser = asyncHandler(async (req, res) => {
+  try {
+    const projects = await Project.find({ createdBy: req.user.id });
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, projects, "All User Projects found successfully."),
+      );
+  } catch (error) {
+    console.error("Error getting projects:", error);
+    throw new ApiError(500, "Internal server error while getting projects.");
+  }
+});
 
-const getProjectById = asyncHandler(async (req, res) => {});
+const getProjectById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      throw new ApiError(400, "Please provide a valid id");
+    }
 
-const createProject = asyncHandler(async (req, res) => {});
+    const project = await Project.findById(id);
+    if (!project) {
+      throw new ApiError(404, "Project with id not found");
+    }
 
-const updateProject = asyncHandler(async (req, res) => {});
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, project, "Project with id found successfully."),
+      );
+  } catch (error) {
+    console.error("Error getting project by id:", error);
+    throw new ApiError(
+      500,
+      "Internal server error while getting project with id.",
+    );
+  }
+});
 
-const deleteProject = asyncHandler(async (req, res) => {});
+const createProject = asyncHandler(async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!name || !description) {
+      throw new ApiError(400, "Please provide all required fields");
+    }
+
+    const project = await Project.create({
+      name,
+      description,
+      createdBy: req.user.id,
+    });
+    if (!project) {
+      throw new ApiError(400, "Project creation failed.");
+    }
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, project, "Project created successfully."));
+  } catch (error) {
+    console.error("Error creating project:", error);
+    throw new ApiError(500, "Internal server error while creating project.");
+  }
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const { id } = req.params;
+    if (!id) {
+      throw new ApiError(400, "Please provide a valid id");
+    }
+
+    const project = await Project.findById(id);
+    if (!project) {
+      throw new ApiError(404, "Project with id not found");
+    }
+
+    project.name = name;
+    project.description = description;
+    await project.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, project, "Project updated successfully."));
+  } catch (error) {
+    console.error("Error updating project:", error);
+    throw new ApiError(500, "Internal server error while updating project.");
+  }
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      throw new ApiError(400, "Please provide a valid id");
+    }
+
+    const project = await Project.findByIdAndDelete(id);
+    if (!project) {
+      throw new ApiError(404, "Project with id not found");
+    }
+    
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "Project deleted successfully."));
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    throw new ApiError(500, "Internal server error while deleting project.");
+  }
+});
 
 const addMemberToProject = asyncHandler(async (req, res) => {});
 
@@ -21,7 +127,7 @@ const updateMemberRole = asyncHandler(async (req, res) => {});
 const deleteMemberRole = asyncHandler(async (req, res) => {});
 
 export {
-  getProject,
+  getAllProjectsOfUser,
   getProjectMembers,
   getProjectById,
   deleteMemberRole,
