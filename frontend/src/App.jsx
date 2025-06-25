51cb317b419fe4fec5b44d268814c3a6620fcece
raheelhparekh <rhparekh2003@gmail.com>
2025-06-25 20:53:18 +0530

    feat : frontend setup with daisy ui + react + vite + tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Routes, Route, Navigate} from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+       <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+      
+    </>
+  )
+}
+
+export default App
