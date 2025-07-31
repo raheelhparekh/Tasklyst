3bce6da173dbe8570692cb9e0011c64acf05ba14
raheelhparekh <rhparekh2003@gmail.com>
2025-07-31 18:20:18 +0530

    fix: update start script and correct environment variable for allowed origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ dotenv.config({
 
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? [process.env.FRONTEND_URL]
+    ? [process.env.BASE_URL]
     : ["http://localhost:5173", "http://localhost:3000"];
 
 app.use(
